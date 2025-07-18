614a8586557ba7d9ba1515632db44c2435f3106a
jendermine <jendermine@gmail.com>
2025-07-18 16:34:23 +0530

    postcall

--- java/com/android/dialer/main/impl/OldMainActivityPeer.java
+++ java/com/android/dialer/main/impl/OldMainActivityPeer.java
@@ -410,7 +410,7 @@ public class OldMainActivityPeer implements MainActivityPeer, FragmentUtilListen
     boolean forceUpdate =
         !LocaleUtils.getLocale(activity).getISO3Language().equals(savedLanguageCode);
     Database.get(activity).getDatabaseHelper(activity).startSmartDialUpdateThread(forceUpdate);
-    showPostCallPrompt();
+    //showPostCallPrompt();
 
     if (searchController.isInSearch()
         || callLogAdapterOnActionModeStateChangedListener.isActionModeStateEnabled()) {
